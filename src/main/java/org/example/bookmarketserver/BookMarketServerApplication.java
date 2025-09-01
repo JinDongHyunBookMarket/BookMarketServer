a4b4b6a829de44b6b3a81a78485064b36c53ee24
kingjinyong <wlsdydwhswkf@gmail.com>
2025-09-01 16:26:24 +0900

    first commit

---
+++ src/main/java/org/example/bookmarketserver/BookMarketServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookmarketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMarketServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMarketServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bookmarketserver/BookMarketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookmarketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMarketServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
